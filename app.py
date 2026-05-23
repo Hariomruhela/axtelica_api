0f97be7b38c717950ec04c01845c6b2af5401b7a
Hariomruhela <it21.hariomruhela@svceindore.ac.in>
2026-05-23 18:55:36 +0530

    update the email massage

--- app.py
+++ app.py
@@ -309,9 +309,6 @@ def customer_auto_response(name):
             <tr>
               <td style="padding:30px;
               background:linear-gradient(90deg,#2563eb,#9333ea);
-              background:#4f46e5;
-              bgcolor="#4f46e5";
-                              
               color:#ffffff;
               text-align:center;">
 
